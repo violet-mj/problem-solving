5828de849edda738c4f6bc22f6aa42f0574a00ce
mjmj <violet_prog@naver.com>
2023-07-20 23:54:23 +0900

    solve 5337

---
+++ baekjoon/problem/5337.py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+output = sys.stdout.write
+
+def solve():
+    print(".  .   .")
+    print("|  | _ | _. _ ._ _  _")
+    print("|/\|(/.|(_.(_)[ | )(/.")
+
+if __name__ == "__main__":
+    solve()
