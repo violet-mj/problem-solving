3b541e562663257208c152f4688b97a6d4e269d7
mjmj <violet_prog@naver.com>
2023-10-18 23:30:23 +0900

    baekjoon: 14929(c++)

---
+++ baekjoon/problem/14929.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "/Users/mj/.vim/template/debug.cpp"
+#else
+#define debug(x...)
+#endif
+
+typedef long long lint;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<lint, lint> pll;
+typedef vector<int> vi;
+typedef vector<lint> vl;
+
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define endl "\n"
+#define all(v) (v).begin(),(v).end()
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vector<int> x(n+1);
+	vector<int> pre(n+1);
+	for(int i = 1; i <= n; i++) {
+		cin >> x[i];
+		pre[i] = pre[i-1] + x[i];
+	}
+
+
+	lint res = 0LL;
+
+	for(int i = 1; i <= n; i++) {
+		res += (lint)x[i] * (pre[n] - pre[i]);
+	}
+
+	cout << res << "\n";
+}
+
+int main() {
+	cin.tie(0);
+  ios::sync_with_stdio(0);
+	int t;
+	// cin >> t;
+	// for(int i = 0; i < t; i++) 
+	solve();
+}
